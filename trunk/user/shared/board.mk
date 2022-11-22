39497f47dde16de524e4c28e7f81588ad851e0c8
李水明 <lism95@midea.com>
2022-11-22 11:21:59 +0800

    test buil-MT7628.yml

--- trunk/user/shared/board.mk
+++ trunk/user/shared/board.mk
@@ -1,8 +1,8 @@
 ##################################################################
 # Board PID # Board Name       # PRODUCT # Note
 ##################################################################
-# RT-N56U   # ASUS RT-N56U     # RT3883  #
+# MI-NANO   # Xiaomi Mi Nano   # MT7628  #
 ##################################################################
 
-CFLAGS += -DBOARD_N56U -DVENDOR_ASUS
-BOARD_NUM_USB_PORTS=2
+CFLAGS += -DBOARD_MINANO
+BOARD_NUM_USB_PORTS=0
